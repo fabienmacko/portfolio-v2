e08d5dd883e4fd975fdde8975b8e112f122dc44f
fabienmacko <fmackodev@gmail.com>
2020-02-05 10:49:06 +0100

    debug resize height

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,11 +9,13 @@ import './App.scss';
 
 class App extends React.Component {
 
+
   componentDidMount() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     // We listen to the resize event
     window.addEventListener('resize', () => {
       // We execute the same script as before
-      let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
     AOS.init();
